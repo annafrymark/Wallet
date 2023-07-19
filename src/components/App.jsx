8730386fd8776a99f3606643b0efc4c574f13252
Michał Pawłowski <107926477+mpawlowski98@users.noreply.github.com>
2023-07-19 16:22:09 +0200

    upp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import Header from './shared/Header';
-
+import Modal from './ModallAddTransaction/ModalAddTransaction';
 export const App = () => {
   return (
     <div>
       <Header />
+      <Modal />
       Project Wallet Group 6
     </div>
   );
