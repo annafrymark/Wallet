024658e16a21e99d5744b93b234c53cc60c4b33b
EwaMazurek <ewa.mmazurek@gmail.com>
2023-07-24 17:15:28 +0200

    transaction list, currencies and balance with css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,45 @@
 // import useAuth from "hooks/useAuth";
-import React, { lazy } from "react";
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
-import { refreshUser } from "redux/auth/authOperations";
-import { RestrictedRoute } from "./Routes/RestrictedRoute";
-import { Suspense } from "react";
+import React, { lazy } from 'react';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { refreshUser } from 'redux/auth/authOperations';
+import { RestrictedRoute } from './Routes/RestrictedRoute';
+import { Suspense } from 'react';
+import { TransactionsList } from './TransactionsTable/TransactionsTable';
+import { Balance } from './Balance/Balance';
+import { CurrencyTable } from './Currencies/Currencies';
 
-const RegistrationPage = lazy(() => import('../pages/Registration/RegistrationPage'));
-
+const RegistrationPage = lazy(() =>
+  import('../pages/Registration/RegistrationPage')
+);
 
 export const App = () => {
   const dispatch = useDispatch();
   // const { isRefreshing } = useAuth();
 
   useEffect(() => {
-        dispatch(refreshUser());
+    dispatch(refreshUser());
   }, [dispatch]);
 
   return (
-
-      <Routes>
-        <Route path='/register'
-          element={
-            <Suspense fallback={<div> Lodaing...</div>}>
-              <RestrictedRoute redirectTo='/' component={<RegistrationPage />} />
-              </Suspense>
-          }
-        ></Route>
-      </Routes>
-
-    // <div
-    //   style={{
-    //     height: '100vh',
-    //     display: 'flex',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     fontSize: 40,
-    //     color: '#010101',
-    //   }}
-    // >
-    //   Project Wallet Group 6
-    // </div>
+    <Routes>
+      <Route
+        path="/register"
+        element={
+          <Suspense fallback={<div> Lodaing...</div>}>
+            <RestrictedRoute redirectTo="/" component={<RegistrationPage />} />
+          </Suspense>
+        }
+      ></Route>
+      <Route
+        path="/home"
+        element={
+          <Suspense fallback={<div> Lodaing...</div>}>
+            <RestrictedRoute redirectTo="/" component={<CurrencyTable />} />
+          </Suspense>
+        }
+      ></Route>
+    </Routes>
   );
 };
