be1c83e76cd15d550723c06970312a1fff61992e
Dawid Czyzewski <vinciebr@proton.me>
2023-07-25 19:49:36 +0200

    fix/mui loader on visable

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,14 @@
-import { FallingLines } from 'react-loader-spinner';
+import React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/joy/CircularProgress';
 
-export const Loader = () => {
-  return (
-    <>
-      <FallingLines
-        color="#4fa94d"
-        width="100"
-        visible={true}
-        ariaLabel="falling-lines-loading"
-      />
-    </>
-  );
+export const Loader = ({ visable }) => {
+  return visable ? (
+    <Backdrop
+      sx={{ color: '#fff', zIndex: theme => theme.zIndex.drawer + 1 }}
+      open={visable}
+    >
+      <CircularProgress color="inherit" />
+    </Backdrop>
+  ) : null;
 };
-
