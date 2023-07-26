776b0632d5e88f0e732d1f7035be0199f49fee7f
Iceonq <bartek.sosnowicz@gmail.com>
2023-07-26 16:27:42 +0200

    .

---
+++ src/components/ModalLogout/ModalLogout.jsx
@@ -0,0 +1,21 @@
+import css from './ModalLogout.module.css';
+
+const ModalLogout = ({ setShowModal }) => {
+  const handleCancel = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <div className={css.backdrop}>
+      <div className={css.modal}>
+        <p className={css.modalTitle}>Are you sure you want to sign out?</p>
+        <button className={css.modalButton}>YES</button>
+        <button onClick={handleCancel} className={css.modalButton__cancel}>
+          NO
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ModalLogout;
