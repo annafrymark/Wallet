5beefcb6642e03720780513a3f1ba7a48897c4df
annafrymark <anna.frymark@gmail.com>
2023-08-01 07:36:26 +0200

    store rename and base url verified

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { Provider } from 'react-redux';
-import { persistor, store } from 'redux/storeReg';
+import { persistor, store } from 'redux/store';
 import './index.css';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
