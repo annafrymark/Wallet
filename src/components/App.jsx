2942f079ab5b054650c4974d298f9d076667687c
annafrymark <anna.frymark@gmail.com>
2023-08-01 14:40:38 +0200

    routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { refreshUser } from 'redux/auth/authOperations';
 import { RestrictedRoute } from './Routes/RestrictedRoute';
+import { PrivateRoute } from './Routes/PrivateRoute';
 import { Suspense } from 'react';
 import { Loader } from './Loader/Loader';
 import { DashboardPage } from '../pages/DashboardPage/DashboardPage';
@@ -14,33 +15,57 @@ import { CurrencyTable } from './Currencies/Currencies';
 import { Diagram } from './DashBoard/Statistics/Diagram';
 import Header from './shared/Header';
 import Modal from './ModallAddTransaction/ModalAddTransaction';
+import { useAuth } from "hooks/useAuth";
+
 const LoginPage = lazy(() => import('../pages/LoginPage/LoginPage'));
 
 const RegistrationPage = lazy(() =>
   import('../pages/RegistrationPage/RegistrationPage')
 );
 
-// import useAuth from "hooks/useAuth";
 
 export const App = () => {
   const dispatch = useDispatch();
-  // const { isRefreshing } = useAuth();
+  const { isRefreshing } = useAuth();
+  
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  return (
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
     <div>
       <Suspense fallback={<Loader />}>
-      <Modal />
+        {/* <Modal /> */}
         <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/register" element={<RegistrationPage />} />
+          <Route
+            path="/"
+            element={
+              <RestrictedRoute redirectTo="/home" component={<LoginPage />} />
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                redirectTo="/"
+                component={<RegistrationPage />}
+              />
+            }
+          />
+          <Route
+            path="/home"
+            element={
+              <PrivateRoute component={<DashboardPage />}>
+                <Route path="diagram" element={<Diagram />} />
+                <Route path="currencies" element={<CurrencyTable />} />
+              </PrivateRoute>
+            }
+          />
+
           {/* <Route path="/" element={<Header />} /> */}
-          <Route path="/home" element={<DashboardPage />}>
-            <Route path="diagram" element={<Diagram />} />
-            <Route path="currencies" element={<CurrencyTable />} />
-          </Route>
+          
         </Routes>
       </Suspense>
     </div>
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,14 @@
+//import React from 'react';
+//import { Helmet } from 'react-helmet-async';
 import css from './LoginPage.module.css';
 import LoginForm from 'components/LoginForm/LoginForm';
 
 const LoginPage = () => {
   return (
     <div className={css.container}>
+      {/* <Helmet>
+        <title>Wallet</title>
+      </Helmet> */}
       <LoginForm />
     </div>
   );
