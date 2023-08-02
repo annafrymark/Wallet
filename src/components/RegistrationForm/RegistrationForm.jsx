b40a5617fec7792da744ab20bfcfb5c177bdea19
annafrymark <anna.frymark@gmail.com>
2023-08-02 17:17:24 +0200

    restore

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -32,7 +32,7 @@ const userSchema = Yup.object({
 });
 
 export const RegisterForm = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch(); // eslint-disable-next-line
   const navigate = useNavigate(); // eslint-disable-next-line
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
@@ -40,46 +40,26 @@ export const RegisterForm = () => {
 
   const handlePasswordVisibility = () => {
     setShowPassword(!showPassword);
-    console.log('setshow');
   };
 
   const handleConfirmPasswordVisibility = () => {
     setShowConfirmPassword(!showConfirmPassword);
-    console.log('setconfirm');
   };
 
   const handleSubmit = event => {
     event.preventDefault();
-    const form = event.currentTarget;
 
-    dispatch(
-      register({
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-        confirmPassword :form.elements.confirmPassword.value,
-        name: form.elements.name.value,
-      })
-    );
+    const form = event.currentTarget;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+    const confirmPassword = form.elements.confirmPassword.value;
+    const firstName = form.elements.name.value;
+
+    dispatch(register({ email, password, confirmPassword, firstName }));
+    console.log(email, password, confirmPassword, firstName);
+    // navigate('/');
     form.reset();
   };
-
-
-
-  // const handleSubmit = event => {
-  //   event.preventDefault();
-
-  //   const form = event.currentTarget;
-  //   const email = form.elements.email.value;
-  //   const password = form.elements.password.value;
-  //   const confirmPassword = form.elements.confirmPassword.value;
-  //   const name = form.elements.name.value;
-
-  //   dispatch(register({ email, password, confirmPassword, name }));
-  //   // console.log('cos cos cos');
-  //   console.log(email, password, confirmPassword, name);
-  //   navigate('/');
-  //   form.reset();
-  // };
 
   return (
     <Formik
@@ -206,7 +186,7 @@ export const RegisterForm = () => {
                   <VisibilityIcon style={{ color: '#e0e0e0' }} />
                 )}
               </span>
-              <PasswordStrengthBar password={values.password} />
+              <PasswordStrengthBar password={password} />
             </label>
             <label className={css.Field}>
               {touched.name && errors.name ? (
