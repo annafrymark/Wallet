52b468c4e23c95fe39e9dc18758d41b0f36d827a
PaulinaStachPluskowska <109916798+PaulinaStachPluskowska@users.noreply.github.com>
2023-08-03 03:04:37 +0200

    summary

---
+++ src/pages/NotFound.js/NotFoundPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Media from "react-media";
+import picture from '../../utils/images/frame-cashappPNF404.png';
+import css from './NotFoundPage.module.css';
+
+export const NotFound = () => { 
+    return (
+        <div className={css.Container}>
+            <Media query='(min-width: 767px)'
+                render={() => (
+                    <div className={css.LogoContainer}>
+                        <img className={css.Image} src={picture} alt='' />
+                    </div>
+                )}
+            />
+        </div>
+    );
+};
